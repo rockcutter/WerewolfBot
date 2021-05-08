890e328180e4cda4df72108a4fb58c6f249f8185
rockcutter <rockcutter414@gmail.com>
2021-05-08 18:55:42 +0900

    add: gameクラスにstopメソッドを追加
    ゲームを停止させる

--- WerewolfBot/myModule/game.py
+++ WerewolfBot/myModule/game.py
@@ -8,8 +8,13 @@ class game(object):
     
 
     def __init__(self):
-        status = PHASE_RECEPTION
+        self.InitGame()
         return
+
+    def InitGame(self):
+        member = []
+        status = PHASE_RECEPTION
+        return 
 
     def Start(self):
         if(status & PHASE_INPROGRESS == PHASE_INPROGRESS):
@@ -17,8 +22,17 @@ class game(object):
         status = PHASE_INPROGRESS
         return True
 
-    
+    def Stop(self):
+        if(status & PHASE_RECEPTION == PHASE_RECEPTION):
+            return False
+        status = PHASE_RECEPTION
+        self.InitGame()
+        return True
 
+
+    #概要: メンバーを追加する関数
+    #引数: メンバーを示す文字列
+    #戻り値: 無し
     def AddMember(self,str):
         self.member.append(str)
         return
