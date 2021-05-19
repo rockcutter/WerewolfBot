042591d1149c91dc58c470d50676357485b8bc7a
rockcutter <rockcutter414@gmail.com>
2021-05-20 03:28:48 +0900

    add: ActivateGame関数 StartGame関数を追加
    status変更系メソッドが必要だったため

--- WerewolfBot/MyModule/ManagerModule/GameManager.py
+++ WerewolfBot/MyModule/ManagerModule/GameManager.py
@@ -1,6 +1,6 @@
-STATUS_NOTACTIVATED = 0x00000000
-STATUS_RECEPTION = 0x00010000
-STATUS_INPROGRESS = 0x00100000
+STATUS_NOTACTIVATED =   0x00000000
+STATUS_RECEPTION =      0x00010000
+STATUS_INPROGRESS =     0x00100000
 
 
 class GameManager(object):
@@ -18,5 +18,13 @@ class GameManager(object):
     def CheckStatus(self):
         return self.gameStatus
 
+    #status変更メソッド--------------------------------
+    def ActivateGame(self):
+        self.gameStatus = STATUS_RECEPTION
+        return
+
+    def StartGame(self):
+        self.gameStatus = STATUS_INPROGRESS
+        return
 
 
