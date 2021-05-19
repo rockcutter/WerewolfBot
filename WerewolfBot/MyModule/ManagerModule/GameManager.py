eece5ec35b53b8990f13398d966919629706d84e
rockcutter <rockcutter414@gmail.com>
2021-05-20 03:17:09 +0900

    add: GameManagerクラスを追加
    ゲームの進行を管理させるため

---
+++ WerewolfBot/MyModule/ManagerModule/GameManager.py
@@ -0,0 +1,22 @@
+STATUS_NOTACTIVATED = 0x00000000
+STATUS_RECEPTION = 0x00010000
+STATUS_INPROGRESS = 0x00100000
+
+
+class GameManager(object):
+    """Gameの進行を管理する"""
+    gameStatus = NOTACTIVATED
+
+    def __init__(self):
+        self.ManuInit()
+        return
+
+    def ManuInit(self):
+        self.gameStatus = NOTACTIVATED
+        return
+
+    def CheckStatus(self):
+        return self.gameStatus
+
+
+
